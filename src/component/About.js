ea1e7fbed9b9366ddc62df964fa73bc1fb34f6a3
Irfan Zahran <fanzahran123@gmail.com>
2020-12-30 04:01:40 +0700

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -8,7 +8,7 @@ function About() {
             <h1>Depo Tanaman</h1>
         </div>
         <div className="about-content">
-            <p> Selamat datang di website resmi Depo Tanaman. Depo Tanaman merupakan distributor bibt buah dengan kualitas unggul, terpercaya, dan harga terjangkau.</p>
+            <p> Selamat datang di website resmi Depo Tanaman. Depo Tanaman merupakan distributor bibit buah dengan kualitas unggul, terpercaya, dan harga terjangkau.</p>
 <p> Temukan berbagai bibit buah terbaik dengan harga terjangkau hanya di Depo Tanaman. Anda bisa membelinya melalui website, Tokopedia, Shopee, atau bisa membeli langsung ke Toko kami yang berada di jalan Mangu-Sambi Km.4, Gelaran, Sobokerto, Ngemplak, Boyolali, Jawa Tengah, Indonesia.</p>
         </div>
       </div>
