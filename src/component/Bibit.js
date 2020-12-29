c50aa5fe6f93c78e32328ae6f159b0e1097d414f
Irfan Zahran <fanzahran123@gmail.com>
2020-12-30 04:02:40 +0700

    Update Bibit.js

--- src/component/Bibit.js
+++ src/component/Bibit.js
@@ -1,30 +1,115 @@
-import React from 'react'
-import {Card, Button} from "react-bootstrap"
-
+import React from "react";
+import "./../App.css";
+import { Card, Form, Col, Container } from "react-bootstrap";
+import Alpukat from "./../img/alpukat.jpg";
 
 function Bibit() {
-    return (
-        <div className='bibit' id="bibit">
-            <div className="container">
+  return (
+    <div className="bibit" id="bibit">
+      <Container fluid="sm">
         <div className="bibit-judul">
-            <h1>Bibit</h1>
+          <h1>Bibit</h1>
         </div>
         <div className="bibit-content">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-        </Card>
+          <Form>
+            <Form.Row>
+              <Col>
+                <Card style={{ border: "none" }}>
+                  <div
+                    className="atas"
+                    style={{
+                      height: "300px",
+                      overflow: "hidden",
+                      borderRadius: "3px",
+                    }}
+                  >
+                    <Card.Img variant="top" src={Alpukat} />
+                  </div>
+                  <Card.Body>
+                    <Card.Text>
+                      {/* <p className="namaProduk"  >Bibit Alpukat Tanpa Biji Unggulan</p>
+          <b className="text-success ">Rp 50.000</b> */}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Form.Row>
+            <Form.Row>
+              <Col sm={2}>
+                <Card>
+                  <Card.Img variant="top" src={Alpukat} />
+                  <Card.Body>
+                    <p className="namaProduk">
+                      Bibit Alpukat Tanpa Biji Unggulan
+                    </p>
+                    <b className="text-success ">Rp 50.000</b>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col sm={2}>
+                <Card>
+                  <Card.Img variant="top" src={Alpukat} />
+                  <Card.Body>
+                    <p className="namaProduk">
+                      Bibit Alpukat Tanpa Biji Unggulan
+                    </p>
+                    <b className="text-success ">Rp 50.000</b>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col sm={2}>
+                <Card>
+                  <Card.Img variant="top" src={Alpukat} />
+                  <Card.Body>
+                    <p className="namaProduk">
+                      Bibit Alpukat Tanpa Biji Unggulan
+                    </p>
+                    <b className="text-success ">Rp 50.000</b>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col sm={2}>
+                <Card>
+                  <Card.Img variant="top" src={Alpukat} />
+                  <Card.Body>
+                    <p className="namaProduk">
+                      Bibit Alpukat Tanpa Biji Unggulan
+                    </p>
+                    <b className="text-success ">Rp 50.000</b>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col sm={2}>
+                <Card>
+                  <Card.Img variant="top" src={Alpukat} />
+                  <Card.Body>
+                    <p className="namaProduk">
+                      Bibit Alpukat Tanpa Biji Unggulan
+                    </p>
+                    <b className="text-success ">Rp 50.000</b>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col sm={2}>
+                <Card>
+                  <Card.Img variant="top" src={Alpukat} />
+                  <Card.Body>
+                    <p className="namaProduk">
+                      Bibit Alpukat Tanpa Biji Unggulan
+                    </p>
+                    <b className="text-success ">Rp 50.000</b>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Form.Row>
+          </Form>
         </div>
-      </div>
+        <div className="bibit-footer">
+          <button className="lebihBanyak">Lihat lebih banyak...</button>
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
 
-export default Bibit
+export default Bibit;
