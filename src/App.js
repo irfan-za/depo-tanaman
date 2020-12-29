1efb3fc965957b0d7bb6018b6c773ae50eb97b1c
Irfan Zahran <fanzahran123@gmail.com>
2020-12-30 04:20:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navs from "./component/Nav";
 import Banner from "./component/Banner";
 import About from "./component/About";
-import Bibit from "./component/Bibit"
-// import gambar
-import Palete from "./img/a.png";
+import Bibit from "./component/Bibit";
+import Contact from "./component/Contact";
 
 function App() {
   return (
@@ -16,10 +15,7 @@ function App() {
       <Banner />
       <About />
       <Bibit />
-      <br />
-      <br />
-      sduhjagdf
-      <img id="about" style={{ width: "100px" }} src={Palete} alt="" />
+      <Contact />
     </div>
   );
 }
