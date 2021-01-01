a9f3407cdcbd6683d0635e4d1bd65c66be78644d
Irfan Zahran <fanzahran123@gmail.com>
2021-01-01 23:35:05 +0700

    Adding scroll animation
    Adding react on scroll animation and animate.css install the animation with npm install react-animate-on-scroll and npm install animate.css

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "./../App.css";
+// import animasi
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/source/animate.css";
 
 function Banner() {
   return (
     <div className="banner" id="home">
       <div className="container">
-        <p>Selamat datang di website</p>
-        <h1>DEPO TANAMAN</h1>
+        <ScrollAnimation animateIn="lightSpeedInLeft">
+          <p>Selamat datang di website</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="pulse" duration={0.7} delay={200}>
+          <h1>DEPO TANAMAN</h1>
+        </ScrollAnimation>
         <div className="tombol">
-          <a href="#about">
-            <button>Selengkapnya</button>
-          </a>
-          <a href="#kontak">
-            <button>Kontak Kami</button>
-          </a>
+          <ScrollAnimation
+            animateIn="bounceInUp"
+            animateOut="bounceOutDown"
+            delay={300}
+          >
+            <a href="#about">
+              <button>Selengkapnya</button>
+            </a>
+            <a href="#kontak">
+              <button>Kontak Kami</button>
+            </a>
+          </ScrollAnimation>
         </div>
       </div>
     </div>
