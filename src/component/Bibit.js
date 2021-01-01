5ff55360011ad50e179ace72c128fa1ec1986dac
Irfan Zahran <fanzahran123@gmail.com>
2021-01-01 23:35:44 +0700

    Add scroll animation
    Adding react on scroll animation and animate.css install the animation with npm install react-animate-on-scroll and npm install animate.css

--- src/component/Bibit.js
+++ src/component/Bibit.js
@@ -2,110 +2,123 @@ import React from "react";
 import "./../App.css";
 import { Card, Form, Col, Container } from "react-bootstrap";
 import Alpukat from "./../img/alpukat.jpg";
+// import Animasi
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/source/animate.css";
 
 function Bibit() {
   return (
     <div className="bibit" id="bibit">
       <Container fluid="sm">
         <div className="bibit-judul">
-          <h1>Bibit</h1>
+          <ScrollAnimation
+            animateIn="zoomIn"
+            animateOut="zoomOut"
+            duration={0.4}
+          >
+            <h1>Bibit</h1>
+          </ScrollAnimation>
         </div>
         <div className="bibit-content">
-          <Form>
-            <Form.Row>
-              <Col>
-                <Card style={{ border: "none" }}>
-                  <div
-                    className="atas"
-                    style={{
-                      height: "300px",
-                      overflow: "hidden",
-                      borderRadius: "3px",
-                    }}
-                  >
-                    <Card.Img variant="top" src={Alpukat} />
-                  </div>
-                  <Card.Body>
-                    <Card.Text>
-                      {/* <p className="namaProduk"  >Bibit Alpukat Tanpa Biji Unggulan</p>
+          <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" delay={100}>
+            <Form>
+              <Form.Row>
+                <Col>
+                  <Card style={{ border: "none" }}>
+                    <div
+                      className="atas"
+                      style={{
+                        height: "300px",
+                        overflow: "hidden",
+                        borderRadius: "3px",
+                      }}
+                    >
+                      <Card.Img variant="top" src={Alpukat} />
+                    </div>
+                    <Card.Body>
+                      <Card.Text>
+                        {/* <p className="namaProduk"  >Bibit Alpukat Tanpa Biji Unggulan</p>
           <b className="text-success ">Rp 50.000</b> */}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Form.Row>
-            <Form.Row>
-              <Col sm={2}>
-                <Card>
-                  <Card.Img variant="top" src={Alpukat} />
-                  <Card.Body>
-                    <p className="namaProduk">
-                      Bibit Alpukat Tanpa Biji Unggulan
-                    </p>
-                    <b className="text-success ">Rp 50.000</b>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col sm={2}>
-                <Card>
-                  <Card.Img variant="top" src={Alpukat} />
-                  <Card.Body>
-                    <p className="namaProduk">
-                      Bibit Alpukat Tanpa Biji Unggulan
-                    </p>
-                    <b className="text-success ">Rp 50.000</b>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col sm={2}>
-                <Card>
-                  <Card.Img variant="top" src={Alpukat} />
-                  <Card.Body>
-                    <p className="namaProduk">
-                      Bibit Alpukat Tanpa Biji Unggulan
-                    </p>
-                    <b className="text-success ">Rp 50.000</b>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col sm={2}>
-                <Card>
-                  <Card.Img variant="top" src={Alpukat} />
-                  <Card.Body>
-                    <p className="namaProduk">
-                      Bibit Alpukat Tanpa Biji Unggulan
-                    </p>
-                    <b className="text-success ">Rp 50.000</b>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col sm={2}>
-                <Card>
-                  <Card.Img variant="top" src={Alpukat} />
-                  <Card.Body>
-                    <p className="namaProduk">
-                      Bibit Alpukat Tanpa Biji Unggulan
-                    </p>
-                    <b className="text-success ">Rp 50.000</b>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col sm={2}>
-                <Card>
-                  <Card.Img variant="top" src={Alpukat} />
-                  <Card.Body>
-                    <p className="namaProduk">
-                      Bibit Alpukat Tanpa Biji Unggulan
-                    </p>
-                    <b className="text-success ">Rp 50.000</b>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Form.Row>
-          </Form>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Form.Row>
+              <Form.Row>
+                <Col sm={2}>
+                  <Card>
+                    <Card.Img variant="top" src={Alpukat} />
+                    <Card.Body>
+                      <p className="namaProduk">
+                        Bibit Alpukat Tanpa Biji Unggulan
+                      </p>
+                      <b className="text-success ">Rp 50.000</b>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col sm={2}>
+                  <Card>
+                    <Card.Img variant="top" src={Alpukat} />
+                    <Card.Body>
+                      <p className="namaProduk">
+                        Bibit Alpukat Tanpa Biji Unggulan
+                      </p>
+                      <b className="text-success ">Rp 50.000</b>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col sm={2}>
+                  <Card>
+                    <Card.Img variant="top" src={Alpukat} />
+                    <Card.Body>
+                      <p className="namaProduk">
+                        Bibit Alpukat Tanpa Biji Unggulan
+                      </p>
+                      <b className="text-success ">Rp 50.000</b>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col sm={2}>
+                  <Card>
+                    <Card.Img variant="top" src={Alpukat} />
+                    <Card.Body>
+                      <p className="namaProduk">
+                        Bibit Alpukat Tanpa Biji Unggulan
+                      </p>
+                      <b className="text-success ">Rp 50.000</b>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col sm={2}>
+                  <Card>
+                    <Card.Img variant="top" src={Alpukat} />
+                    <Card.Body>
+                      <p className="namaProduk">
+                        Bibit Alpukat Tanpa Biji Unggulan
+                      </p>
+                      <b className="text-success ">Rp 50.000</b>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col sm={2}>
+                  <Card>
+                    <Card.Img variant="top" src={Alpukat} />
+                    <Card.Body>
+                      <p className="namaProduk">
+                        Bibit Alpukat Tanpa Biji Unggulan
+                      </p>
+                      <b className="text-success ">Rp 50.000</b>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Form.Row>
+            </Form>
+          </ScrollAnimation>
         </div>
         <div className="bibit-footer">
-          <button className="lebihBanyak">Lihat lebih banyak...</button>
+          <ScrollAnimation animateIn="slideInUp" animateOut="slideOutDown">
+            <button className="lebihBanyak">Lihat lebih banyak...</button>
+          </ScrollAnimation>
         </div>
       </Container>
     </div>
