6496b7887421815b35a81ac1e55cdc12ed56a7e0
Irfan Zahran <fanzahran123@gmail.com>
2021-01-01 23:33:26 +0700

    Add scroll animation
    Adding react on scroll animation and animate.css
    
    install the animation with npm install react-animate-on-scroll and npm install animate.css

--- src/component/About.js
+++ src/component/About.js
@@ -1,15 +1,37 @@
 import React from "react";
+// import animasi
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/source/animate.css";
 
 function About() {
   return (
     <div className="about" id="about">
       <div className="container">
         <div className="about-judul">
+          <ScrollAnimation
+            animateIn="zoomIn"
+            animateOut="zoomOut"
+            duration={0.4}
+          >
             <h1>Depo Tanaman</h1>
+          </ScrollAnimation>
         </div>
+
         <div className="about-content">
-            <p> Selamat datang di website resmi Depo Tanaman. Depo Tanaman merupakan distributor bibit buah dengan kualitas unggul, terpercaya, dan harga terjangkau.</p>
-<p> Temukan berbagai bibit buah terbaik dengan harga terjangkau hanya di Depo Tanaman. Anda bisa membelinya melalui website, Tokopedia, Shopee, atau bisa membeli langsung ke Toko kami yang berada di jalan Mangu-Sambi Km.4, Gelaran, Sobokerto, Ngemplak, Boyolali, Jawa Tengah, Indonesia.</p>
+          <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
+            <p>
+              Selamat datang di website resmi Depo Tanaman. Depo Tanaman
+              merupakan distributor bibit buah dengan kualitas unggul,
+              terpercaya, dan harga terjangkau.
+            </p>
+            <p>
+              Temukan berbagai bibit buah terbaik dengan harga terjangkau hanya
+              di Depo Tanaman. Anda bisa membelinya melalui website, Tokopedia,
+              Shopee, atau bisa membeli langsung ke Toko kami yang berada di
+              jalan Mangu-Sambi Km.4, Gelaran, Sobokerto, Ngemplak, Boyolali,
+              Jawa Tengah, Indonesia.
+            </p>
+          </ScrollAnimation>
         </div>
       </div>
     </div>
